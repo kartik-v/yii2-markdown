facb892dd44e292bb6751581dc1bc3690a3e4be8
Kartik Visweswaran <kartikv2@gmail.com>
2014-01-16 08:59:06 +0530

    Updated German Translations

--- markdown/messages/de/markdown.php
+++ markdown/messages/de/markdown.php
@@ -53,5 +53,5 @@ return array(
     'URL/Link' => 'URL/Link',
     'Your {type} file will be generated. Save the file to your client with {ext} extension in the accompanying dialog.' => 'Ihre {type}-Datei wird erzeugt. Speichern Sie die Datei auf Ihrem Rechner mit der Erweiterung .htm/.html im nachfolgenden Dialog.',
     '{bullet} You may use {link} syntax.' => '{bullet} Sie können die {link} Syntax nutzen.',
-    '{bullet} To undo / redo, press CTRL-Z / CTRL-Y. You can also undo most button actions by clicking it again.' => '{bullet} Rückgängig machen/Wiederherstellen  via STRG-Z / STRG-Y. Die meisten Button-Aktionen können Sie auch durch erneutes Anklicken rückgängig machen.',
+    '{bullet} To undo / redo, press CTRL-Z / CTRL-Y. You can also undo most button actions by clicking it again.' => '{bullet} Rückgängig machen/Wiederherstellen via STRG-Z/STRG-Y. Die meisten Button-Aktionen können Sie auch durch erneutes Anklicken rückgängig machen.',
 );
