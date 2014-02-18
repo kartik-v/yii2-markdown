89c5456102d3059fc404912f1e14dd1123638c81
Kartik Visweswaran <kartikv2@gmail.com>
2014-02-18 23:41:48 +0530

    Asset bundle enhanced for asset manager config

--- markdown/MarkdownEditorAsset.php
+++ markdown/MarkdownEditorAsset.php
@@ -8,25 +8,20 @@
 
 namespace kartik\markdown;
 
-use yii\web\AssetBundle;
-
 /**
  * Asset bundle for MarkdownEditor Widget
  *
  * @author Kartik Visweswaran <kartikv2@gmail.com>
  * @since 1.0
  */
-class MarkdownEditorAsset extends AssetBundle {
+class MarkdownEditorAsset extends AssetBundle
+{
 
-    public $depends = [
-        'yii\web\JqueryAsset',
-        'yii\bootstrap\BootstrapAsset',
-    ];
-
-    public function init() {
-        $this->sourcePath = __DIR__ . '/../assets';
-        $this->css = YII_DEBUG ? ['css/kv-markdown.css'] : ['css/kv-markdown.min.css'];
-        $this->js = YII_DEBUG ? ['js/rangyinputs-jquery-1.1.2.js', 'js/kv-markdown.js'] : ['js/rangyinputs-jquery-1.1.2.min.js', 'js/kv-markdown.min.js'];
+    public function init()
+    {
+        $this->setSourcePath(__DIR__ . '/../assets');
+        $this->setupAssets('css', ['css/kv-markdown']);
+        $this->setupAssets('js', ['js/rangyinputs-jquery-1.1.2', 'js/kv-markdown']);
         parent::init();
     }
 
