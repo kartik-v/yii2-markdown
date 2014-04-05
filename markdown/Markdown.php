4c594fb407a1abdc8dbb630cfe73bdd195a2f9bb
Kartik Visweswaran <kartikv2@gmail.com>
2014-04-05 16:34:06 +0530

    Reformatted as per Yii2 coding standards

--- markdown/Markdown.php
+++ markdown/Markdown.php
@@ -22,87 +22,87 @@ use yii\base\InvalidConfigException;
 class Markdown
 {
 
-	/**
-	 * @var MarkdownExtra
-	 */
-	protected static $markdown;
+    /**
+     * @var MarkdownExtra
+     */
+    protected static $markdown;
 
-	// SmartyPantsTypographer does nothing at all
-	const SMARTYPANTS_ATTR_DO_NOTHING = 0;
-	// "--" for em-dashes; no en-dash support
-	const SMARTYPANTS_ATTR_EM_DASH = 1;
-	// "---" for em-dashes; "--" for en-dashes
-	const SMARTYPANTS_ATTR_LONG_EM_DASH_SHORT_EN = 2;
-	// "--" for em-dashes; "---" for en-dashes
-	const SMARTYPANTS_ATTR_SHORT_EM_DASH_LONG_EN = 3;
+    // SmartyPantsTypographer does nothing at all
+    const SMARTYPANTS_ATTR_DO_NOTHING = 0;
+    // "--" for em-dashes; no en-dash support
+    const SMARTYPANTS_ATTR_EM_DASH = 1;
+    // "---" for em-dashes; "--" for en-dashes
+    const SMARTYPANTS_ATTR_LONG_EM_DASH_SHORT_EN = 2;
+    // "--" for em-dashes; "---" for en-dashes
+    const SMARTYPANTS_ATTR_SHORT_EM_DASH_LONG_EN = 3;
 
-	/**
-	 * Converts markdown into HTML
-	 *
-	 * @param string $content
-	 * @param array $config . Options to configure MarkdownExtra and smarty
-	 * - markdown: array for MarkdownExtra configuration parameters
-	 * - smarty: array for SmartyPantsTypographer configuration parameters
-	 * - custom: array for Custom configuration parameters
-	 * @param int $smartyMode the SmartyPantsTypographer processing mode
-	 * @return string
-	 * @throws InvalidConfigException if module not set
-	 */
-	public static function convert($content, $config = [], $smartyMode = self::SMARTYPANTS_ATTR_LONG_EM_DASH_SHORT_EN)
-	{
-		$module = \Yii::$app->getModule('markdown');
-		if ($module === null) {
-			throw new InvalidConfigException("The module 'markdown' was not found. Ensure you have setup the 'markdown' module in your Yii configuration file.");
-		}
-		$output = $content;
-		if (strlen($output) > 0) {
-			$mdConfig = empty($config['markdown']) ? [] : $config['markdown'];
-			$output = static::process($content, $mdConfig);
-			if ($module->smartyPants) {
-				$smConfig = empty($config['smarty']) ? [] : $config['smarty'];
-				$smarty = new SmartyPantsTypographer($smartyMode);
-				foreach ($smConfig as $name => $value) {
-					$smarty->{$name} = $value;
-				}
-				$output = $smarty->transform($output);
-				$cuConfig = empty($config['custom']) ? $module->customConversion : $config['custom'];
-				$output = static::customProcess($output, $cuConfig);
-			}
-		}
-		return $output;
-	}
+    /**
+     * Converts markdown into HTML
+     *
+     * @param string $content
+     * @param array $config . Options to configure MarkdownExtra and smarty
+     * - markdown: array for MarkdownExtra configuration parameters
+     * - smarty: array for SmartyPantsTypographer configuration parameters
+     * - custom: array for Custom configuration parameters
+     * @param int $smartyMode the SmartyPantsTypographer processing mode
+     * @return string
+     * @throws InvalidConfigException if module not set
+     */
+    public static function convert($content, $config = [], $smartyMode = self::SMARTYPANTS_ATTR_LONG_EM_DASH_SHORT_EN)
+    {
+        $module = \Yii::$app->getModule('markdown');
+        if ($module === null) {
+            throw new InvalidConfigException("The module 'markdown' was not found. Ensure you have setup the 'markdown' module in your Yii configuration file.");
+        }
+        $output = $content;
+        if (strlen($output) > 0) {
+            $mdConfig = empty($config['markdown']) ? [] : $config['markdown'];
+            $output = static::process($content, $mdConfig);
+            if ($module->smartyPants) {
+                $smConfig = empty($config['smarty']) ? [] : $config['smarty'];
+                $smarty = new SmartyPantsTypographer($smartyMode);
+                foreach ($smConfig as $name => $value) {
+                    $smarty->{$name} = $value;
+                }
+                $output = $smarty->transform($output);
+                $cuConfig = empty($config['custom']) ? $module->customConversion : $config['custom'];
+                $output = static::customProcess($output, $cuConfig);
+            }
+        }
+        return $output;
+    }
 
-	/**
-	 * Converts markdown into HTML
-	 *
-	 * @param string $content
-	 * @param array $config
-	 * @return string
-	 */
-	public static function process($content, $config = [])
-	{
-		if (static::$markdown === null) {
-			static::$markdown = new MarkdownExtra();
-		}
-		foreach ($config as $name => $value) {
-			static::$markdown->{$name} = $value;
-		}
-		return static::$markdown->transform($content);
-	}
+    /**
+     * Converts markdown into HTML
+     *
+     * @param string $content
+     * @param array $config
+     * @return string
+     */
+    public static function process($content, $config = [])
+    {
+        if (static::$markdown === null) {
+            static::$markdown = new MarkdownExtra();
+        }
+        foreach ($config as $name => $value) {
+            static::$markdown->{$name} = $value;
+        }
+        return static::$markdown->transform($content);
+    }
 
-	/**
-	 * Custom conversion of patterns
-	 *
-	 * @param string $content
-	 * @param array $config . List of key value pairs to find and replace
-	 * @return string
-	 */
-	public static function customProcess($content, $config = [])
-	{
-		if (empty($config)) {
-			return $content;
-		}
-		return strtr($content, $config);
-	}
+    /**
+     * Custom conversion of patterns
+     *
+     * @param string $content
+     * @param array $config . List of key value pairs to find and replace
+     * @return string
+     */
+    public static function customProcess($content, $config = [])
+    {
+        if (empty($config)) {
+            return $content;
+        }
+        return strtr($content, $config);
+    }
 
 }
--- markdown/controllers/ParseController.php
+++ markdown/controllers/ParseController.php
@@ -16,45 +16,45 @@ use kartik\markdown\Markdown;
 class ParseController extends \yii\web\Controller
 {
 
-	/**
-	 * Convert markdown text to HTML for preview
-	 *
-	 * @returns JSON encoded HTML output
-	 */
-	public function actionPreview()
-	{
-		$output = '';
-		$module = Yii::$app->controller->module;
-		if (isset($_POST['source'])) {
-			$output = (strlen($_POST['source']) > 0) ? Markdown::convert($_POST['source'], ['custom' => $module->customConversion]) : $_POST['nullMsg'];
-		}
-		echo Json::encode(HtmlPurifier::process($output));
-	}
+    /**
+     * Convert markdown text to HTML for preview
+     *
+     * @returns JSON encoded HTML output
+     */
+    public function actionPreview()
+    {
+        $output = '';
+        $module = Yii::$app->controller->module;
+        if (isset($_POST['source'])) {
+            $output = (strlen($_POST['source']) > 0) ? Markdown::convert($_POST['source'], ['custom' => $module->customConversion]) : $_POST['nullMsg'];
+        }
+        echo Json::encode(HtmlPurifier::process($output));
+    }
 
-	/**
-	 * Download the exported file
-	 */
-	public function actionDownload()
-	{
-		$type = empty($_POST['export_filetype']) ? 'htm' : $_POST['export_filetype'];
-		$name = empty($_POST['export_filename']) ? Yii::t('kvgrid', 'export') : $_POST['export_filename'];
-		$content = empty($_POST['export_content']) ? Yii::t('kvgrid', 'No data found') : $_POST['export_content'];
-		$this->setHttpHeaders($type, $name);
-		return $content;
-	}
+    /**
+     * Download the exported file
+     */
+    public function actionDownload()
+    {
+        $type = empty($_POST['export_filetype']) ? 'htm' : $_POST['export_filetype'];
+        $name = empty($_POST['export_filename']) ? Yii::t('kvgrid', 'export') : $_POST['export_filename'];
+        $content = empty($_POST['export_content']) ? Yii::t('kvgrid', 'No data found') : $_POST['export_content'];
+        $this->setHttpHeaders($type, $name);
+        return $content;
+    }
 
-	/**
-	 * Sets the HTTP headers needed by file download action.
-	 */
-	protected function setHttpHeaders($type, $name)
-	{
-		$mime = ($type == 'htm') ? 'text/html' : 'text/plain';
-		Yii::$app->getResponse()->getHeaders()
-				 ->set('Pragma', 'public')
-				 ->set('Expires', '0')
-				 ->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0')
-				 ->set('Content-Disposition', 'attachment; filename="' . $name . '.' . $type . '"')
-				 ->set('Content-type', $mime . '; charset=utf-8');
-	}
+    /**
+     * Sets the HTTP headers needed by file download action.
+     */
+    protected function setHttpHeaders($type, $name)
+    {
+        $mime = ($type == 'htm') ? 'text/html' : 'text/plain';
+        Yii::$app->getResponse()->getHeaders()
+                 ->set('Pragma', 'public')
+                 ->set('Expires', '0')
+                 ->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0')
+                 ->set('Content-Disposition', 'attachment; filename="' . $name . '.' . $type . '"')
+                 ->set('Content-type', $mime . '; charset=utf-8');
+    }
 
 }
